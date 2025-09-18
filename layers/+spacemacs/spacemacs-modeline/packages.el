9cefbe4c7d16db5ef27c6a2a8dc34d7eba840aa5
خالد رافغ <khalidrafi.twzde@slmail.me>
2025-09-18 08:16:57 +0600

    Change spaceline repository

--- layers/+spacemacs/spacemacs-modeline/packages.el
+++ layers/+spacemacs/spacemacs-modeline/packages.el
@@ -25,7 +25,10 @@
       '(
         (doom-modeline :toggle (eq (spacemacs/get-mode-line-theme-name) 'doom))
         fancy-battery
-        (spaceline :toggle (spacemacs//enable-spaceline-p))
+        (spaceline :location (recipe
+                              :fetcher github
+                              :repo "khalidrafi6/spaceline-android")
+                   :toggle (spacemacs//enable-spaceline-p))
         (spaceline-all-the-icons :toggle (eq (spacemacs/get-mode-line-theme-name) 'all-the-icons))
         symon
         (powerline :toggle (eq (spacemacs/get-mode-line-theme-name) 'vim-powerline))
